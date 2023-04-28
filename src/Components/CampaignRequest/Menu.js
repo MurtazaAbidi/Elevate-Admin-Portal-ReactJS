90e039b01f385d333ce359353507b57d7b4a5b02
Murtaza Abidi <murtaza2476175@gmail.com>
2023-04-28 12:35:57 +0500

    campaign request integration done

--- src/Components/CampaignRequest/Menu.js
+++ src/Components/CampaignRequest/Menu.js
@@ -1,24 +1,28 @@
 import React, { useState } from "react";
 import './menu-style.css'
 
-const Menu = ({ items, setReasonModalOpen, setModalOpen, setDataForModal }) => {
+const Menu = ({ items, setReasonModalOpen, setModalOpen, setDataForModal, loading }) => {
   const [done, setDone] = useState([])
   return (
     <div className="section-center">
-      {items.map((item) => {
+      {
+        loading ?
+        <div style={{ height: '100vh' }} className="lds-roller"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+          :
+          items.map((item) => {
         if (done.includes(item.id)) {
           return null;
         }
-        const { id, title, img, desc } = item;
+        const { campaign_id, campaign_title,campaigner_name, campaign_type, campaign_image, campaign_description, progress, days_left, likes } = item;
         return (
-          <article style={{marginLeft:'-5rem', cursor:"revert"}} key={id} className="menu-item">
-            <img src={img} alt={title} className="photo" />
+          <article style={{marginLeft:'-5rem', cursor:"revert"}} key={campaign_id} className="menu-item">
+            <img src={campaign_image} alt={campaign_title} className="photo" />
             <div style={{width:'30rem'}} className="item-info">
               <header>
-                <h4>{title}</h4>
-                <h4 className="price">{item.category}</h4>
+              <h4>{campaign_title}<span style={{color:'#a6a6a6'}}>{" (by: "+campaigner_name+")"}</span></h4>
+                <h4 className="price">{campaign_type}</h4>
               </header>
-              <p className="item-text">{desc}</p>
+              <p className="item-text">{campaign_description}</p>
               {/* <div style={{textAlign:'right', margin:'5px', }}><span style={{ padding:'5px 23px',borderRadius:30, color:'#4267B2', fontWeight:700}}>Likes: {like}</span></div> */}
               {/* <ProgressBar progress={progress} height={25} /> */}
               <button onClick={()=>{setDone([...done, item.id]); }} className="inside-menu-button" style={{border:'2px solid #04F004', background:'rgb(4,240,4,0.07)'}} >Accept</button>
--- src/Pages/CampaignRequest/NewCampaignRequest.jsx
+++ src/Pages/CampaignRequest/NewCampaignRequest.jsx
@@ -1,26 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Categories from "../../Components/CampaignRequest/Categories";
 import Menu from "../../Components/CampaignRequest/Menu";
 import items from "../../Components/CampaignRequest/dashboardData.js";
 import "./style.css";
 import Modal from "../../Components/CampaignDetails";
 import ReasonModal from "../../Components/ReasonModal";
+import axios from "axios";
 
-const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+const allCategories = ["all", "equity", "reward", "profit", "donation"];
 
 const NewCampaignRequest = () => {
-  const [menuItems, setMenuItems] = useState(items);
+  const [items, setItems] = useState([])
+  const [menuItems, setMenuItems] = useState([]);
   const [activeCategory, setActiveCategory] = useState("");
   const [modalOpen, setModalOpen] = useState(false);
   const [dataForModal, setDataForModal] = useState({})
   const [reasonModalOpen, setReasonModalOpen] = useState(false);
   const [rejectionReasonData, setRejectionReasonData] = useState([])
+  const [loading, setLoading] = useState(false);
+
 
   const categories = allCategories;
 
-  // useEffect(()=>{
-  //   console.log(rejectionReasonData)
-  // },[rejectionReasonData])
+  useEffect(() => {
+    setLoading(true);
+    axios.get(
+      // body: JSON.stringify({
+      `${process.env.REACT_APP_API_URL}/api/admin/campaignrequests`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        withCredentials: true,
+      }
+    )
+      .then(function (response) {
+        console.log(response.data);
+        setMenuItems(response.data)
+        setItems(response.data)
+        setLoading(false);
+      })
+      .catch(function (error) {
+        console.log(error.response.data.msg);
+        alert(error.response.data.msg);
+        setLoading(false);
+      });
+  }, [])
 
   const filterItems = (category) => {
     setActiveCategory(category);
@@ -29,13 +55,13 @@ const NewCampaignRequest = () => {
       return;
     }
 
-    const newItems = items.filter((item) => item.category === category);
+    const newItems = items.filter((item) => item.campaign_type === category);
     setMenuItems(newItems);
   };
   return (
     <>
       {reasonModalOpen && <ReasonModal rejectionReasonData={rejectionReasonData} setRejectionReasonData={setRejectionReasonData} setReasonModalOpen={setReasonModalOpen} />}
-      {modalOpen && <Modal setOpenModal={setModalOpen} dataForModal={dataForModal}/>}
+      {modalOpen && <Modal setOpenModal={setModalOpen} dataForModal={dataForModal} />}
       <div className="myProduct-body">
         <main>
           <section className="section">
@@ -49,7 +75,7 @@ const NewCampaignRequest = () => {
               activeCategory={activeCategory}
               filterItems={filterItems}
             />
-            <Menu items={menuItems} setReasonModalOpen={setReasonModalOpen} setModalOpen={setModalOpen} setDataForModal={setDataForModal}/>
+            <Menu items={menuItems} setReasonModalOpen={setReasonModalOpen} setModalOpen={setModalOpen} setDataForModal={setDataForModal} loading={loading}/>
           </section>
         </main>
       </div>
